c49532720fb4e136cd60a6dcfc7c226df038b394
All4nFernandes <allandossantosfernandes@gmail.com>
2025-04-25 21:47:44 -0400

    Função logar e cadastrar feita.

--- model/LoginModel.php
+++ model/LoginModel.php
@@ -1,5 +1,7 @@
 <?php
 
+//função para fazer login
+
 class LoginModel
 {
     protected $conn;
@@ -7,8 +9,24 @@ class LoginModel
 
     public function __construct()
     {
-
+        $db = New Database();
+        $this-conn = $db->connect();
     }
+
+    public function login($email){
+        $query = "SELECT * FROM $this->tabela WHERE email = :email";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+        return $stmt->rowCount() > 0; // retorna true se for o login funcionar.
+    }
+
+    public function Cadastrar($email){
+        $query = "INSERT INTO $this->tabela('email')VALUES(':email')";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+        return $stmt->rowCount() > 0;
 }
 
 
