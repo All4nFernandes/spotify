5154750974718c7abc4aa8cf90044c67a093b295
All4nFernandes <allandossantosfernandes@gmail.com>
2025-05-09 21:15:12 -0400

    icone add ao botao home. Iniciando barra de pesquisa.

--- view/components/navbar.php
+++ view/components/navbar.php
@@ -2,40 +2,79 @@
 
 <header>
     <nav class="navbar-container">
-        <img class="logo-navbar" src="\spotify\view\assets\img\spotify_logo_branca.png" alt="logo-spotify">
-        <div>
-            <ul>
-                <li>
-                    <!-- button home -->
-                </li>
-                <li>
-                    <!-- button de pesquisa -->
-                </li>
-            </ul>
-        </div>
-        <div>
-            <ul>
-                <li>
-                    <a href=""></a> <!-- link premium -->
-                </li>
-                <li>
-                    <a href=""></a> <!-- link suporte -->
-                </li>
-                <li>
-                    <a href=""></a> <!-- link baixar -->
-                </li>
-                <div id="linha-vertical"></div>
-                <li>
-                    <img src="" alt="icone-baixar"> <!-- svg baixar -->
-                    <a href=""></a> <!-- link instalar aplicativo -->
-                </li>
-                <li>
-                    <a href=""></a> <!-- link inscrever-se -->
-                </li>
-                <li>
-                    <a href=""></a> <!-- btn entrar -->
-                </li>
-            </ul>
+        <div class="box-navbar">
+            <img class="logo-navbar" src="\spotify\view\assets\img\spotify_logo_branca.png" alt="logo-spotify">
+            <div>
+                <ul>
+                    <li>
+                        <button class="btn-home" type="button">
+                            <svg class="svg-home" viewBox="-2 0 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ffffff" stroke="#ffffff">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <title>home [#1392]</title>
+                                    <desc>Created with Sketch.</desc>
+                                    <defs> </defs>
+                                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <g id="Dribbble-Light-Preview" transform="translate(-381.000000, -720.000000)"
+                                            fill="#c2bdbd">
+                                            <g id="icons" transform="translate(56.000000, 160.000000)">
+                                                <path
+                                                    d="M339.875,578.013 L336.6875,578.013 L336.6875,574.013 L330.3125,574.013 L330.3125,578.013 L327.125,578.013 L327.125,568.799 L333.489375,562.809 L339.875,568.819 L339.875,578.013 Z M341.94475,568.013 L333.47025,560 L325,567.999 L325,580.013 L332.4375,580.013 L332.4375,576.013 L334.5625,576.013 L334.5625,580.013 L342,580.013 L342,579.983 L342,568.013 L341.94475,568.013 Z"
+                                                    id="home-[#1392]"> </path>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </button><!-- button home -->
+                    </li>
+                    <li>
+                        <input type="text" class="barra-pesquisa" <svg fill="#c2bdbd" height="200px" width="200px"
+                            version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"
+                            stroke="#c2bdbd">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <g>
+                                <g>
+                                    <path
+                                        d="M508.255,490.146l-128-128c-0.06-0.06-0.137-0.077-0.196-0.128c34.193-38.434,55.142-88.917,55.142-144.418 c0-120.175-97.425-217.6-217.6-217.6S0.001,97.425,0.001,217.6s97.425,217.6,217.6,217.6c55.501,0,105.975-20.949,144.418-55.151 c0.06,0.06,0.077,0.137,0.128,0.196l128,128c2.5,2.509,5.777,3.755,9.054,3.755s6.554-1.246,9.054-3.746 C513.247,503.253,513.247,495.147,508.255,490.146z M217.601,409.6c-105.865,0-192-86.135-192-192s86.135-192,192-192 s192,86.135,192,192S323.466,409.6,217.601,409.6z">
+                                    </path>
+                                </g>
+                            </g>
+                        </g></svg>>
+
+                        <!-- button de pesquisa -->
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a href=""></a> <!-- link premium -->
+                    </li>
+                    <li>
+                        <a href=""></a> <!-- link suporte -->
+                    </li>
+                    <li>
+                        <a href=""></a> <!-- link baixar -->
+                    </li>
+                    <div id="linha-vertical"></div>
+                    <li>
+                        <img src="" alt="icone-baixar"> <!-- svg baixar -->
+                        <a href=""></a> <!-- link instalar aplicativo -->
+                    </li>
+                    <li>
+                        <a href=""></a> <!-- link inscrever-se -->
+                    </li>
+                    <li>
+                        <a href=""></a> <!-- btn entrar -->
+                    </li>
+                </ul>
+            </div>
         </div>
 
     </nav>
